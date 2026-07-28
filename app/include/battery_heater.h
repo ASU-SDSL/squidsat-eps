12a99dd29535a5b18df16c3fd0e8295cfd4e4e0a
Doyle-Squared <adoyle28@asu.edu>
2026-07-28 12:14:10 -0700

    Added descriptions to battery heater header file, as well as function call checks in the .c file

--- app/include/battery_heater.h
+++ app/include/battery_heater.h
@@ -1,10 +1,30 @@
+/**
+ * @file battery_heater.h
+ * @author Aidan Doyle (Doyle-Squared)
+ * @brief   Header file for battery heater gpio toggling functions
+ * @version 0.1
+ * @date 2026-07-28
+ */
 #ifndef BATTERY_HEATER_H
 #define BATTERY_HEATER_H
 
+#include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
+#include <zephyr/sys/util.h>
 #include <zephyr/drivers/gpio.h>
 
+/**
+ * @brief Sets the battery heater CTRL gpio to high/on
+ * 
+ * @return int - Returns 1 if toggled, 0 if toggle fails
+ */
 int battHeaterOn();
 
+/**
+ * @brief Sets the battery heater CTRL gpio to low/off
+ * 
+ * @return int Returns 1 if toggled, 0 if toggle fails
+ */
 int battHeaterOff();
 
 #endif
--- app/src/battery_heater.c
+++ app/src/battery_heater.c
@@ -10,15 +10,25 @@
 
 #include "battery_heater.h"
 
+LOG_MODULE_REGISTER(batteryHeater, LOG_LEVEL_INF);
+
 #define BATT_HEATER DT_ALIAS(rail6)
 static const struct gpio_dt_spec batteryHeater = GPIO_DT_SPEC_GET(BATT_HEATER, gpios);
 
 int battHeaterOn(){
-    gpio_pin_set_dt(&batteryHeater, 1);
-    return 1;
+    if(!gpio_pin_set_dt(&batteryHeater, 1)){
+        LOG_INF("Battery heater has been turned on");
+        return 1;
+    };
+    LOG_WRN("Battery Heater failed to turn on");
+    return 0;
 }
 
 int battHeaterOff(){
-    gpio_pin_set_dt(&batteryHeater, 0);
-    return 1;
+    if(!gpio_pin_set_dt(&batteryHeater, 0)){
+        LOG_INF("Battery heater has been turned off");
+        return 1;
+    };
+    LOG_WRN("Battery Heater failed to turn ff");
+    return 0;
 }
